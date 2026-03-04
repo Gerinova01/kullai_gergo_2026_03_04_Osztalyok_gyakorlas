dd11decc7aaf6d202fda78023d24ddc18178bfe4
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2026-03-04 09:00:31 +0100

    feladat3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from auto import Auto
+import random
 
 auto1 = Auto("Toyota", "Yaris", 2008)
 auto2 = Auto("Ford", "Focus", 2018)
@@ -6,7 +7,24 @@ auto3 = Auto("Volvo", "s60", 2006)
 auto4 = Auto("Alfa Romeo", "Giulia", 2007)
 auto5 = Auto("Chevrolet", "Lacetti", 2017)
 
-auto1.gyorsit(150)
-auto1.fekez(30)
-print(auto1)
+# auto1.gyorsit(150)
+# auto1.fekez(30)
+# print(auto1)
 
+autok = [auto1, auto2, auto3, auto4, auto5]
+for auto in autok:
+    print(auto)
+
+print("\nGyorsít\n")
+for auto in autok:
+    auto.gyorsit(random.randint(30, 150))
+    print(auto)
+
+autok_szama = len(autok)
+ossz_eletkor = 0
+for auto in autok:
+    kor = 2026 - auto.gyartasi_ev
+    ossz_eletkor += kor
+
+atlag_eletkor = ossz_eletkor / autok_szama
+print(f"Az autók átlagéletkora {atlag_eletkor} év")
