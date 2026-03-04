66f60c37882c08009099b9188cc61e14fdef87b2
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2026-03-04 10:01:47 +0100

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,9 +1,12 @@
 class Auto:
-    def __init__(self, marka_, tipus_, gyartasi_ev_):
+
+    def __init__(self, marka_, tipus_, gyartasi_ev_, uzemanyag_=50, fogyasztas_=7.5):
         self.marka = marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev_
         self.sebesseg = 0
+        self.uzemanyag = uzemanyag_
+        self.fogyasztas = fogyasztas_
 
     def __str__(self):
         return f"{self.marka} {self.tipus} {self.gyartasi_ev}, sebesség = {self.sebesseg} km/h"
@@ -18,4 +21,8 @@ class Auto:
         if self.sebesseg < 0:
             self.sebesseg = 0
     
-    
+
+    def tankol(self, mennyiseg):
+        self.uzemanyag += mennyiseg
+        if self.uzemanyag > 50:
+            self.uzemanyag = 50
