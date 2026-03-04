caff1c4d40b1172ac24edeb6f7f2dc22d999e60f
Gerinova01 <kullai.gergo@diak.szbi-pg.hu>
2026-03-04 08:37:06 +0100

    gyorsitas fekezes

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from auto import Auto
 
-auto = Auto("Toyota", "Yaris", 2008)
-auto = Auto("Ford", "Focus", 2018)
+auto1 = Auto("Toyota", "Yaris", 2008)
+auto2 = Auto("Ford", "Focus", 2018)
+auto3 = Auto("Volvo", "s60", 2006)
+auto4 = Auto("Alfa Romeo", "Giulia", 2007)
+auto5 = Auto("Chevrolet", "Lacetti", 2017)
+
+auto1.gyorsit(150)
+auto1.fekez(30)
+print(auto1)
+
